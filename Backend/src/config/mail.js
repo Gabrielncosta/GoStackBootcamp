87f23381bc9622feebac5a6a060abf7a375fe53e
Gabrielncosta <gabrielncosta208@gmail.com>
2020-03-02 10:25:45 -0300

    NodemailerTemplates

---
+++ Backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "dd5bd3fb2283a8",
+    pass: "9c7c3d94a9c1a3"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+};
